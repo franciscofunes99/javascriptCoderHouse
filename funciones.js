02c77334f45f7d4f25c31d5c5e25721426d7209e
franciscofunes99 <francisco.funes93@gmail.com>
2022-08-11 20:29:12 -0300

    Primera entrega proyecto final

--- funciones.js
+++ funciones.js
@@ -1,18 +1,63 @@
+const veladores = [
+    { nombre: "Star Wars", valor: 1500 },
+    { nombre: "Los simpsons", valor: 1500 },
+    { nombre: "Friends", valor: 1500 },
+    { nombre: "River plate", valor: 1500 },
+];
 
-let cantidad = prompt("Cuantos productos deseas comprar?");
-let precio = 1500;
-let total = cantidad * precio;
-
-if (total >= 3000){
-    alert("Tienes envio gratis!")
-    console.log("El valor total es de " + total + " pesos");
-} else if(total <3000){
-   alert("Tienes que abonar el envio!");
-   console.log("El valor total es de " + calcularEnvio() + " pesos");
+function valorTotal() {
+    let valorFinal = carro.reduce(((accumulator, carro) => accumulator + carro.valor), 0);
+    return valorFinal;
 }
 
-function calcularEnvio (){
-    let totalEnvio = total + 650;
-    return totalEnvio;
+let carro = [];
+
+let compra = prompt("Desea comprar algun velador?")
+
+while (compra != "si" && compra != "no") {
+    alert("Elija una opcion SI/NO")
+    compra = prompt("Desea comprar un velador?")
+}
+
+if (compra == "si") {
+    alert("Elige uno de los siguientes veladores:")
+    let veladoresDisponibles = veladores.map((velador) => velador.nombre + " Precio: $" + velador.valor);
+    alert(veladoresDisponibles.join(" - "))
+} else if (compra == "no") {
+    alert("gracias por su visita")
+}
+
+while (compra != "no") {
+    let veladores = prompt("Cual velador deseas comprar?")
+    let precio = 0
+
+    if (veladores == "Star wars" || veladores == "Los simpsons" || veladores == "Friends" || veladores == "River plate") {
+        switch (veladores) {
+            case "Star wars":
+                valor = 1500;
+                break;
+            case "Los simpsons":
+                valor = 1500;
+                break;
+            case "Friends":
+                valor = 1500;
+                break;
+            case "River plate":
+                valor = 1500;
+                break;
+            default:
+                break;
+        }
+        carro.push({ veladores, valor })
+        console.log(carro)
+    }
+    compra = prompt("Quiere continuar con la compra?")
+     
+    while (compra == "no") {
+        alert("Gracias por su compra")
+        alert("El valor final es " + " $" + valorTotal())
+        break;
+
+    }
 }
 
